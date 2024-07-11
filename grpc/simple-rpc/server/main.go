bafc945eaa22162bfbf5c5222cb7e32598ea365c
ilovealt <ilovealt@163.com>
2024-07-11 08:48:41 +0800

    unary拦截器

--- grpc/simple-rpc/server/main.go
+++ grpc/simple-rpc/server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -9,9 +12,20 @@ import (
 	pb "github.com/ilovealt/goinaction/grpc/simple-rpc/ecommerce"
 )
 
+// 创建一个拦截其
+// 函数名称自定义，参数和返回值需要按规定写
+func orderUnaryServerInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+	s := time.Now()
+	// Invoking the handler to complete the normal execution of a unary RPC.
+	m, err := handler(ctx, req)
+	// Post processing logic
+	log.Printf("Method: %s, req: %s, resp: %s, latency: %s\n", info.FullMethod, req, m, time.Now().Sub(s))
+	return m, err
+}
+
 func main() {
 	// 测试环境，取消安全凭证
-	s := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+	s := grpc.NewServer(grpc.Creds(insecure.NewCredentials()), grpc.UnaryInterceptor(orderUnaryServerInterceptor))
 
 	pb.RegisterOrderManagementServer(s, &OrderManagementImpl{})
 
