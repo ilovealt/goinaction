11498df7d257a942b2b630630fc890ee207011e1
ilovealt <ilovealt@163.com>
2024-07-12 09:13:08 +0800

    添加客户端拦截器

--- grpc/simple-rpc/client/main.go
+++ grpc/simple-rpc/client/main.go
@@ -12,9 +12,21 @@ import (
 	pb "github.com/ilovealt/goinaction/grpc/simple-rpc/ecommerce"
 )
 
+func orderUnaryClientInterceptor(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	s := time.Now()
+	// 方法调用
+	err := invoker(ctx, method, req, reply, cc, opts...)
+
+	log.Printf("Clinet method: %s, req: %s, resp: %s, latency: %s\n", method, req, reply, time.Now().Sub(s))
+
+	return err
+}
+
 func main() {
 	// 测试环境，取消安全凭证
-	conn, err := grpc.NewClient("127.0.0.1:8009", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("127.0.0.1:8009", grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(orderUnaryClientInterceptor))
+
 	if err != nil {
 		panic(err)
 	}
